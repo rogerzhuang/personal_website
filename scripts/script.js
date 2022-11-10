a18aa720772e355f09c63b6686c8e507dbe49636
Roger Zhuang <zhuangziyin@gmail.com>
2022-11-09 20:55:21 -0800

    loop through the array and add course if needed

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,21 @@ const courseList = [
 ];
 
 let input = prompt("Please input 4-digit course number");
-
 while (isNaN(Number(input)) || Math.floor(Number(input)/1000) < 1) {
     alert("The input is invalid, either not a number or less than 4-digit");
     input = prompt("Please input 4-digit course number");
+}
+
+let isInList = false;
+for (let course of courseList) {
+    if (course.code.includes(input)) {
+        console.log(`Yes I am taking the course: ${course.code} – ${course.name}`);
+        isInList = true;
+        break;
+    }
+}
+
+if (!isInList) {
+    courseList.push({code:input, name:null});
+    console.log(`Add course ${input} successful`);
 }
